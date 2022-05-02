fd0b9cf579d96900364c41351150dcd0d465a48d
Adrian (macbook) <adfraser@ucsc.edu>
2022-05-02 10:38:04 -0700

    Adds hydro options to extract_and_save_data_from_DNS.py

--- python/extract_and_save_data_from_DNS.py
+++ python/extract_and_save_data_from_DNS.py
@@ -8,19 +8,29 @@ import OUTfile_reader
 Pr = 1e-1
 # R0s = np.array([1.45, 3.0, 5.0, 7.0, 9.0])
 R0s = np.array([1.5, 3.0, 5.0, 7.0, 9.0])
-HB = 0.1
-Pm = 0.1
+HB = 0.0
+Pm = 0.01
 
-names = ["R0", "urms", "TEMPrms", "CHEMrms", "Brms", "flux_Temp", "flux_Chem", "u_max(3)",
-         "uxrms", "uyrms", "uzrms", "Bxrms", "Byrms", "Bzrms", "diss_Temp", "diss_Chem"]
+if HB > 0:
+    names = ["R0", "urms", "TEMPrms", "CHEMrms", "Brms", "flux_Temp", "flux_Chem", "u_max(3)",
+             "uxrms", "uyrms", "uzrms", "Bxrms", "Byrms", "Bzrms", "diss_Temp", "diss_Chem"]
+else:
+    names = ["R0", "urms", "TEMPrms", "CHEMrms", "flux_Temp", "flux_Chem", "u_max(3)",
+             "uxrms", "uyrms", "uzrms", "diss_Temp", "diss_Chem"]
 
 data_array = np.zeros((len(R0s), len(names)), dtype=np.float64)
 for ri, r0 in enumerate(R0s):
     data_array[ri, 0] = r0
     for ni, name in enumerate(names[1:]):
-        data_array[ri, ni+1] = OUTfile_reader.get_avg_from_DNS(Pr, r0, HB, Pm, name)
+        if HB > 0:
+            data_array[ri, ni+1] = OUTfile_reader.get_avg_from_DNS(Pr, r0, HB, Pm, name)
+        else:
+            data_array[ri, ni + 1] = OUTfile_reader.get_avg_from_hydr_DNS(r0, name)
 
-fname = 'extracted_data/Pr{}_HB{}_Pm{}_R0scan_data.txt'.format(Pr, HB, Pm)
+if HB > 0:
+    fname = 'extracted_data/Pr{}_HB{}_Pm{}_R0scan_data.txt'.format(Pr, HB, Pm)
+else:
+    fname = 'extracted_data/Pr{}_HB{}_R0scan_data.txt'.format(Pr, HB)
 
 np.savetxt(fname, data_array, header=' '.join(names))
 
